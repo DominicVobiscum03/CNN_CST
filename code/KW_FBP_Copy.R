a616de6b866c021ac9748ea6f7592a43b948a9e8
Micah-Sterk <micah.sterk@hope.edu>
2024-06-04 13:55:50 -0400

    64x64, add bias

--- code/KW_FBP_Copy.R
+++ code/KW_FBP_Copy.R
@@ -1,12 +1,13 @@
 library(PET)
 library(png)
+knitr::opts_knit$set(root.dir='Z:/Research/ANN Work')
 
 #Read in the Sinograms
-filt1 <- read.csv("raw_data/filtsinoa-KW.csv")
+filt1 <- read.csv("raw_data/filtsinoa.csv")
 filt1 <- as.matrix(filt1)
 image(filt1)
 
-filt2 <- read.csv("raw_data/filtsinob-KW.csv")
+filt2 <- read.csv("raw_data/filtsinob.csv")
 filt2 <- as.matrix(filt2)
 image(filt2)
 
@@ -14,36 +15,36 @@ image(filt2)
 
 # Back Projection
 
-FBP1 <- iradon(filt1, XSamples = 256, YSamples = 256, mode = "BF")
+FBP1 <- iradon(filt1, XSamples = 64, YSamples = 64, mode = "BF")
 filt1data <- FBP1$irData
 image(filt1data)
 
-FBP2 <- iradon(filt2, XSamples = 256, YSamples = 256, mode = "BF")
+FBP2 <- iradon(filt2, XSamples = 64, YSamples = 64, mode = "BF")
 filt2data <- FBP2$irData
 image(filt2data)
 
 
 # Read in the biases
-m1 <- as.matrix(read.csv("raw_data/m1-KW.csv"))
-m2 <- as.matrix(read.csv("raw_data/m2-KW.csv"))
+m1 <- as.matrix(read.csv("raw_data/m1.csv"))
+m2 <- as.matrix(read.csv("raw_data/m2.csv"))
 
-b1 <- matrix(m1[1,1], 256, 256)
-b2 <- matrix(m1[1,2], 256, 256)
+b1 <- matrix(m1[1,1], 64, 64)
+b2 <- matrix(m1[1,2], 64, 64)
 
 
 w1 <- m2[2,1]
 w2 <- m2[3,1]
 
 
-bf <- matrix(m2[1,1], 256, 256)
+bf <- matrix(m2[1,1], 64, 64)
 
 #Subtract biases
 
-filta <- filt1data - b1
-filtb <- filt2data - b2
+filta <- filt1data + b1
+filtb <- filt2data + b2
 
 
-filtsa <- matrix(nrow = 256, ncol = 256)
+filtsa <- matrix(nrow = 64, ncol = 64)
 for (i in 1:nrow(filta)){
   for (j in 1:ncol(filta)){
     filtsa[i,j] <- -1/(1+exp(-filta[i,j]))
@@ -52,7 +53,7 @@ for (i in 1:nrow(filta)){
 
 image(filtsa)
 
-filtsb <- matrix(nrow = 256, ncol = 256)
+filtsb <- matrix(nrow = 64, ncol = 64)
 for (i in 1:nrow(filtb)){
   for (j in 1:ncol(filtb)){
     filtsb[i,j] <- -1/(1+exp(-(filtb[i,j])))
@@ -66,19 +67,19 @@ image(filtsb)
 filtsa <- filtsa*w1
 filtsb <- filtsb*w2
 
-
-
+image(filtsa)
+image(filtsb)
 
 filts <- filtsa + filtsb
 image(filts)
 
 
 
-filts2 <- filts - bf
+filts2 <- filts + bf
 filts2
 image(filts2)
 
-filtf <- matrix(nrow = 256, ncol = 256)
+filtf <- matrix(nrow = 64, ncol = 64)
 for (i in 1:nrow(filts2)){
   for (j in 1:ncol(filts2)){
     filtf[i,j] <- -1/(1+exp(-(filts2[i,j])))
@@ -88,4 +89,4 @@ for (i in 1:nrow(filts2)){
 
 image(filtf)
 
-write.csv(filtf, "clean_data/FBP1-KW.csv")
+write.csv(filtf, "clean_data/FBP1.csv")
