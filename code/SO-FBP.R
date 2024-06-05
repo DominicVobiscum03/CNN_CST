e766b19b358a0a6b7717b55f6471d22623639523
sydneyolander22 <sydney.olander@hope.edu>
2024-06-05 11:06:55 -0400

    s

--- code/SO-FBP.R
+++ code/SO-FBP.R
@@ -14,11 +14,11 @@ image(filt2)
 
 # Back Projection
 
-FBP1 <- iradon(filt1, XSamples = 256, YSamples = 256, mode = "BF")
+FBP1 <- iradon(filt1, XSamples = 64, YSamples = 64, mode = "BF")
 filt1data <- FBP1$irData
 image(filt1data)
 
-FBP2 <- iradon(filt2, XSamples = 256, YSamples = 256, mode = "BF")
+FBP2 <- iradon(filt2, XSamples = 64, YSamples = 64, mode = "BF")
 filt2data <- FBP2$irData
 image(filt2data)
 
@@ -27,15 +27,15 @@ image(filt2data)
 m1 <- as.matrix(read.csv("raw_data/m1-SO.csv"))
 m2 <- as.matrix(read.csv("raw_data/m2-SO.csv"))
 
-b1 <- matrix(m1[1,1], 256, 256)
-b2 <- matrix(m1[1,2], 256, 256)
+b1 <- matrix(m1[1,1], 64, 64)
+b2 <- matrix(m1[1,2], 64, 64)
 
 
 w1 <- m2[2,1]
 w2 <- m2[3,1]
 
 
-bf <- matrix(m2[1,1], 256, 256)
+bf <- matrix(m2[1,1], 64, 64)
 
 #Subtract biases
 
@@ -43,7 +43,7 @@ filta <- filt1data - b1
 filtb <- filt2data - b2
 
 
-filtsa <- matrix(nrow = 256, ncol = 256)
+filtsa <- matrix(nrow = 64, ncol = 64)
 for (i in 1:nrow(filta)){
   for (j in 1:ncol(filta)){
     filtsa[i,j] <- -1/(1+exp(-filta[i,j]))
@@ -52,7 +52,7 @@ for (i in 1:nrow(filta)){
 
 image(filtsa)
 
-filtsb <- matrix(nrow = 256, ncol = 256)
+filtsb <- matrix(nrow = 64, ncol = 64)
 for (i in 1:nrow(filtb)){
   for (j in 1:ncol(filtb)){
     filtsb[i,j] <- -1/(1+exp(-(filtb[i,j])))
@@ -78,7 +78,7 @@ filts2 <- filts - bf
 filts2
 image(filts2)
 
-filtf <- matrix(nrow = 256, ncol = 256)
+filtf <- matrix(nrow = 64, ncol = 64)
 for (i in 1:nrow(filts2)){
   for (j in 1:ncol(filts2)){
     filtf[i,j] <- -1/(1+exp(-(filts2[i,j])))
