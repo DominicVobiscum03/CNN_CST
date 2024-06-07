7623aa78853b49f86c25901e7a522c20c9397e95
sydneyolander22 <sydney.olander@hope.edu>
2024-06-07 15:45:15 -0400

    so

--- code/SO-FBP.R
+++ code/SO-FBP.R
@@ -10,8 +10,9 @@ filt2 <- read.csv("raw_data/filtsinob-SO.csv")
 filt2 <- as.matrix(filt2)
 image(filt2)
 
-filt3 <- read.csv("raw_data/filtsinoc-SO.csv")
-
+#filt3 <- read.csv("raw_data/filtsinoc-SO.csv")
+#filt3 <- as.matrix(filt3)
+#image(filt3)
 
 
 # Back Projection
@@ -24,9 +25,9 @@ FBP2 <- iradon(filt2, XSamples = 64, YSamples = 64, mode = "CNF")
 filt2data <- FBP2$irData
 image(filt2data)
 
-FBP3 <- iradon(filt3, XSamples = 64, YSamples = 64, mode = "CNF")
-filt3data <- FBP3$irData
-image(filt3data)
+#FBP3 <- iradon(filt3, XSamples = 64, YSamples = 64, mode = "CNF")
+#filt3data <- FBP3$irData
+#image(filt3data)
 
 
 # Read in the biases
@@ -35,21 +36,21 @@ m2 <- as.matrix(read.csv("raw_data/m2-SO.csv"))
 
 b1 <- matrix(m1[1,1], 64, 64)
 b2 <- matrix(m1[1,2], 64, 64)
-b3 <- matrix(m1[1,3], 64, 64)
+#b3 <- matrix(m1[1,3], 64, 64)
 
 
 w1 <- m2[2,1]
 w2 <- m2[3,1]
-w3 <- m2[4,1]
+#w3 <- m2[4,1]
 
 
-FB <- matrix(m2[1,1], 64, 64)
+bf <- matrix(m2[1,1], 64, 64)
 
 #Subtract biases
 
 filta <- filt1data + b1
 filtb <- filt2data + b2
-filtc <- filt3data + b3
+#filtc <- filt3data + b3
 
 
 filtsa <- matrix(nrow = 64, ncol = 64)
@@ -69,25 +70,25 @@ for (i in 1:nrow(filtb)){
 }
 image(filtsb)
 
-filtsc <- matrix(nrow = 64, ncol = 64)
-for (i in 1:nrow(filtc)){
-  for (j in 1:ncol(filtc)){
-    filtsc[i,j] <- -1/(1+exp(-(filtc[i,j])))
-  }
-}
-image(filtsc)
+#filtsc <- matrix(nrow = 64, ncol = 64)
+#for (i in 1:nrow(filtc)){
+ # for (j in 1:ncol(filtc)){
+  #  filtsc[i,j] <- -1/(1+exp(-(filtc[i,j])))
+#  }
+#}
+#image(filtsc)
 
 
 
 
 filtsa <- filtsa*w1
 filtsb <- filtsb*w2
-filtsc <- filtsc*w3
+#filtsc <- filtsc*w3
 
 
 
 
-filts <- filtsa + filtsb + filtsc
+filts <- filtsa + filtsb #+ filtsc
 image(filts)
 
 
