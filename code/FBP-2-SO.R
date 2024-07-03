bf43e8a1c65bd01ae202ce14ba7c3fd770df6c45
sydneyolander22 <sydney.olander@hope.edu>
2024-07-03 09:44:41 -0400

    so

--- code/FBP-2-SO.R
+++ code/FBP-2-SO.R
@@ -255,9 +255,7 @@ write.csv(filtf, "clean_data/FBP1-SO-2.csv")
 
 
 
-##Side by side convolutions
-
-##Separate convolutions
+##Side by side separate convolutions
 
 library(PET)
 library(png)
@@ -297,7 +295,7 @@ image(filt2comb)
 # Back Projection
 
 #Filter 1 back projection
-FBP1tot <- iradon(filt1comb, XSamples = 64, YSamples = 64, mode = "BF")
+FBP1tot <- iradon(filt1comb, XSamples = 64, YSamples = 64, mode = "CNF")
 filt1totdata <- FBP1tot$irData
 image(filt1totdata)
 
@@ -369,3 +367,101 @@ heatmap(filtf2,Rowv=NA,Colv=NA,col=paste("gray",1:99,sep=""))
 
 
 write.csv(filtf2, "clean_data/FBP1-SO-2.csv")
+
+
+
+
+## Two full sinos
+
+##Separate convolutions
+
+library(PET)
+library(png)
+
+#Read in the Sinograms
+
+#Filter 1
+filt1a <- read.csv("C:/Users/sumloan/Documents/ANN_work/CNN_CST/raw_data/filtsinoa2-SO.csv")
+filt1a <- as.matrix(filt1a)
+image(filt1a)
+
+#Filter 2
+filt2a <- read.csv("C:/Users/sumloan/Documents/ANN_work/CNN_CST/raw_data/filtsinob2-SO.csv")
+filt2a <- as.matrix(filt2a)
+image(filt2a)
+
+# Back Projection
+
+#Filter 1 back projection
+FBP1tot <- iradon(filt1a, XSamples = 64, YSamples = 64, mode = "BF")
+filt1totdata <- FBP1tot$irData
+image(filt1totdata)
+
+#Filter 2 back projection
+FBP2tot <- iradon(filt2a, XSamples = 64, YSamples = 64, mode = "BF")
+filt2totdata <- FBP2tot$irData
+image(filt2totdata)
+
+# Read in the biases
+m1 <- as.matrix(read.csv("C:/Users/sumloan/Documents/ANN_work/CNN_CST/raw_data/m1-SO-2.csv"))
+m2 <- as.matrix(read.csv("C:/Users/sumloan/Documents/ANN_work/CNN_CST/raw_data/m2-SO-2.csv"))
+
+b1 <- matrix(m1[1,1], 64, 64)
+b2 <- matrix(m1[1,2], 64, 64)
+#b3 <- matrix(m1[1,3], 64, 64)
+
+w1 <- m2[2,1]
+w2 <- m2[3,1]
+#w3 <- m2[4,1]
+
+bf <- matrix(m2[1,1], 64, 64)
+
+#Subtract biases
+
+filta2 <- filt1totdata - b1
+filtb2 <- filt2totdata - b2
+
+
+filtsa2 <- matrix(nrow = 64, ncol = 64)
+for (i in 1:nrow(filta2)){
+  for (j in 1:ncol(filta2)){
+    filtsa2[i,j] <- 1/(1+exp(-filta2[i,j]))
+  }
+}
+
+image(filts2a)
+
+filtsb2 <- matrix(nrow = 64, ncol = 64)
+for (i in 1:nrow(filtb2)){
+  for (j in 1:ncol(filtb2)){
+    filtsb2[i,j] <- 1/(1+exp(-(filtb2[i,j])))
+  }
+}
+image(filtsb2)
+
+filtsa2 <- filtsa2*w1
+filtsb2 <- filtsb2*w2
+
+filts2 <- filtsa2 + filtsb2
+image(filts2)
+
+
+#Remove last bias
+filts3 <- filts2 - bf
+filts3
+image(filts3)
+
+#Last step
+filtf2 <- matrix(nrow = 64, ncol = 64)
+for (i in 1:nrow(filts3)){
+  for (j in 1:ncol(filts3)){
+    filtf2[i,j] <- 1/(1+exp(-(filts3[i,j])))
+  } 
+}
+
+
+image(filtf2)
+
+
+write.csv(filtf2, "clean_data/FBP1-SO-2.csv")
+
